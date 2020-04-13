0ec7896e6605eb53ff0d28eb3dcbf3622c8d98e9
Guosa Edebiri <gedebiri@gmail.com>
2020-04-13 04:19:40 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -3,7 +3,7 @@ def find_max_value(array)
   count = 0 
   max_value = nil
   
-  while count =< array.length do
+  while count < array.length do
     if array[count] < array[count + 1]
       max_value = array[count + 1]
     count += 1
